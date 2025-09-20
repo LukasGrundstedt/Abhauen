d16e37939d72e438e193eb257c922cc7a12be2ff
letusgetLogic <namnguyen.nn604@gmail.com>
2025-09-20 14:44:03 +0200

    Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,11 @@ public class Player : MonoBehaviour
 
     [SerializeField] private SpriteRenderer sprite;
     [SerializeField] private float fallSpeed;
+    [SerializeField] private int aimScore;
     public int MaxHealth = 10;
     public int DrugsAvailable = 5;
 
-
+    public int Score { get; set; } = 0;
     public int Health { get; set; }
     public int DrugsUsed { get; private set; } = 0;
 
@@ -20,10 +21,25 @@ public class Player : MonoBehaviour
         {
             Destroy(Instance.gameObject);
         }
-
         Instance = this;
-        
+
+        Score = aimScore;
         Health = MaxHealth;
+        DrugsUsed = 0;
+        sprite.transform.rotation = Quaternion.identity;
+    }
+
+    private void Update()
+    {
+        Die();
+    }
+
+    public void MakeScore()
+    {
+        if (Score >= aimScore)
+            return;
+
+        Score++;
     }
 
     public void TakeDrugs()
@@ -39,26 +55,23 @@ public class Player : MonoBehaviour
     {
         if (Health <= 0)
             return;
-    }
 
-    public Key GetInput()
-    {
-        return Key.None;
+        Health -= damage;
     }
 
     public void Die()
     {
-        if (GameManager.Instance.State != GameManager.GameState.PlayerIsDying)
+        if (GameManager.Instance.State != GameState.PlayerIsDying)
             return;
 
-        Quaternion target = Quaternion.Euler(0, 0, 90);
+        Quaternion target = Quaternion.Euler(90, 0, 0);
 
         if (Quaternion.Angle(transform.rotation, target) < 0.1f)
         {
-            GameManager.Instance.State = GameManager.GameState.GameOver;
+            GameManager.Instance.State = GameState.GameOver;
             return;
         }
 
-        sprite.transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * fallSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * fallSpeed);
     }
 }
